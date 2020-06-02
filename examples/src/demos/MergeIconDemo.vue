a653df7aab44beaa5716f1681c1c54177309f251
JianhuiChen <chenjianhui0428@gmail.com>
2020-06-02 14:18:14 +0800

    test: add IconPicker and MergeIcon test demo

---
+++ examples/src/demos/MergeIconDemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <merge-icon type="account-book" />
+    <merge-icon type="number" />
+    <merge-icon type="home" :style="{ fontSize: '30px' }"/>
+    <merge-icon type="setting" theme="filled" />
+    <merge-icon type="smile" theme="outlined" />
+    <merge-icon type="custom-danhangshurukuang"/>
+    <merge-icon type="custom-mima"/>
+    <merge-icon type="custom-gongju"/>
+  </div>
+</template>
+<script>
+export default {
+  components: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
